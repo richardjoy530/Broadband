9d3b9ebae98f0d13a619b1b3dc322a1a9f241681
richardjoy530 <richardjoy530@gmail.com>
2020-08-22 13:22:29 +0530

    data fetching working-barebones

---
+++ lib/data.dart
@@ -0,0 +1,24 @@
+Map<String, String> body = {
+  '__LASTFOCUS': '',
+  'ToolkitScriptManager1_HiddenField': '',
+  '__EVENTTARGET': '',
+  '__EVENTARGUMENT': '',
+  '__VIEWSTATE': viewstate,
+  '__VIEWSTATEGENERATOR': viewstategenerator,
+  '__EVENTVALIDATION': eventvalidation,
+  'txtUserName': username,
+  'txtPassword': password,
+  'hdnloginwith': 'username',
+  'save': 'Log In'
+};
+String viewstate;
+String viewstategenerator;
+String eventvalidation;
+String username = 'Please Login';
+String password = 'Please Login';
+String mobNumber = 'Please Login';
+String name = 'Please Login';
+String email = 'Please Login';
+String address = 'Please Login';
+String validityPeriod = 'Please Login';
+String currentUsage = 'Please Login';
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
+import 'package:Broadband/data.dart';
 import 'package:flutter/material.dart';
+import 'package:requests/requests.dart';
+import 'data.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,20 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -33,85 +25,222 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController usernameController;
+  TextEditingController passwordController;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    usernameController = TextEditingController();
+    passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            TextField(
+              controller: usernameController,
+              maxLines: 1,
+              maxLength: 20,
+              decoration: InputDecoration(hintText: 'Username'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            TextField(
+              controller: passwordController,
+              maxLines: 1,
+              maxLength: 20,
+              decoration: InputDecoration(hintText: 'Password'),
             ),
+            RaisedButton(
+              child: Text('Login'),
+              onPressed: () {
+                username = usernameController.text;
+                password = passwordController.text;
+                test();
+                print([usernameController.text, passwordController.text]);
+              },
+            ),
+            Text('Name : $name'),
+            Text('Mobile Number : $mobNumber'),
+            Text('Email Id : $email'),
+            Text('Address : $address'),
+            Text('Validity : $validityPeriod'),
+            Text('Total Usage : $currentUsage'),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<void> test() async {
+    var url = 'https://myaccount.keralavisionisp.com/Customer/Default.aspx';
+    Requests.get(url).then((response) {
+      var viewstateind = response.content().indexOf('__VIEWSTATE');
+      var viewstategeneratorind =
+          response.content().indexOf('__VIEWSTATEGENERATOR');
+      var eventvalidationind = response.content().indexOf('__EVENTVALIDATION');
+      {
+        var viewstatedata =
+            response.content().substring(viewstateind, viewstateind + 4500);
+        int startIndex;
+        int endIndex;
+        for (var i = 0; i < viewstatedata.length; i++) {
+          if (viewstatedata[i] == '/' && startIndex == null) startIndex = i;
+          if (viewstatedata[i] == '"' && endIndex == null && startIndex != null)
+            endIndex = i;
+        }
+        setState(() {
+          viewstate = viewstatedata.substring(startIndex, endIndex);
+        });
+      }
+      {
+        var viewstategeneratordata = response
+            .content()
+            .substring(viewstategeneratorind, viewstategeneratorind + 100);
+        int startIndex = 55;
+        int endIndex = 63;
+        setState(() {
+          viewstategenerator =
+              viewstategeneratordata.substring(startIndex, endIndex);
+        });
+      }
+      {
+        var eventvalidationdata = response
+            .content()
+            .substring(eventvalidationind, eventvalidationind + 300);
+        int startIndex;
+        int endIndex;
+        for (var i = 0; i < eventvalidationdata.length; i++) {
+          if (eventvalidationdata[i] == '/' && startIndex == null)
+            startIndex = i;
+          if (eventvalidationdata[i] == '"' &&
+              endIndex == null &&
+              startIndex != null) endIndex = i;
+        }
+        setState(() {
+          eventvalidation = eventvalidationdata.substring(startIndex, endIndex);
+        });
+      }
+
+      url =
+          'https://myaccount.keralavisionisp.com/Customer/PortalLogin.aspx?h8=1';
+
+      Requests.post(url,
+              body: body, bodyEncoding: RequestBodyEncoding.FormURLEncoded)
+          .then((response) {
+            if(response.statusCode==302){
+
+            
+        url =
+            'https://myaccount.keralavisionisp.com/Customer/HomePage.aspx?Id=147205';
+        Requests.get(url).then((response) {
+          url = 'https://myaccount.keralavisionisp.com/Customer/Gauge.aspx';
+          Requests.get(url).then((response) {
+            var numind = response.content().indexOf('lblMobile');
+            var nameind = response.content().indexOf('lblName');
+            var emailind = response.content().indexOf('lblEmail');
+            var valind = response.content().indexOf('lblValidityPeriod');
+            var currind = response.content().indexOf('lblCurrentUsage');
+            var adddresind = response.content().indexOf('lblAddress');
+            if (numind != -1) { 
+              {
+                var numdata = response.content().substring(numind, numind + 60);
+                int startIndex;
+                int endIndex;
+                for (var i = 0; i < numdata.length; i++) {
+                  if (numdata[i] == '>' && startIndex == null) startIndex = i;
+                  if (numdata[i] == '<' && endIndex == null) endIndex = i;
+                }
+                setState(() {
+                  mobNumber = numdata.substring(startIndex + 1, endIndex);
+                });
+              }
+              {
+                var namedata =
+                    response.content().substring(nameind, nameind + 150);
+                int startIndex;
+                int endIndex;
+                for (var i = 0; i < namedata.length; i++) {
+                  if (namedata[i] == '>' && startIndex == null) startIndex = i;
+                  if (namedata[i] == '<' && endIndex == null) endIndex = i;
+                }
+                setState(() {
+                  name = namedata.substring(startIndex + 1, endIndex);
+                });
+              }
+              {
+                var emaildata =
+                    response.content().substring(emailind, emailind + 70);
+                int startIndex;
+                int endIndex;
+                for (var i = 0; i < emaildata.length; i++) {
+                  if (emaildata[i] == '>' && startIndex == null) startIndex = i;
+                  if (emaildata[i] == '<' && endIndex == null) endIndex = i;
+                }
+                setState(() {
+                  email = emaildata.substring(startIndex + 1, endIndex);
+                });
+              }
+              {
+                var addressdata =
+                    response.content().substring(adddresind, adddresind + 200);
+                int startIndex;
+                int endIndex;
+                for (var i = 0; i < addressdata.length; i++) {
+                  if (addressdata[i] == '>' && startIndex == null)
+                    startIndex = i;
+                  if (addressdata[i] == '<' && endIndex == null) endIndex = i;
+                }
+                setState(() {
+                  address = addressdata.substring(startIndex + 1, endIndex);
+                });
+              }
+              {
+                var valdata = response.content().substring(valind, valind + 50);
+                int startIndex;
+                int endIndex;
+                for (var i = 0; i < valdata.length; i++) {
+                  if (valdata[i] == '>' && startIndex == null) startIndex = i;
+                  if (valdata[i] == '<' && endIndex == null) endIndex = i;
+                }
+                setState(() {
+                  validityPeriod = valdata.substring(startIndex + 1, endIndex);
+                });
+              }
+              {
+                var currdata =
+                    response.content().substring(currind, currind + 50);
+                int startIndex;
+                int endIndex;
+                for (var i = 0; i < currdata.length; i++) {
+                  if (currdata[i] == '>' && startIndex == null) startIndex = i;
+                  if (currdata[i] == '<' && endIndex == null) endIndex = i;
+                }
+                setState(() {
+                  currentUsage = currdata.substring(startIndex + 1, endIndex);
+                });
+              }
+            } else
+              test();
+          });
+        });}
+      });
+    });
   }
 }

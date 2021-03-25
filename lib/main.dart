de08e04bfc1133fc5db37b8eaa776fde21fb1220
Richard Joy <richardjoy530@gmail.com>
2021-03-25 08:58:55 +0530

    - Adding Dark Mode
    - Implementation not finished
    - Color profile made BroadBandTheme

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: BroadBandTheme.themeData,
       home: LoadingPage(),
     );
   }
